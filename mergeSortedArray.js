b0a45e7bd4217227ef7af9d780f8254f082e651b
Sona Binu <sonabinu567@gmail.com>
2026-03-28 10:22:14 +0530

    Add :Q3

---
+++ mergeSortedArray.js
@@ -0,0 +1,10 @@
+var merge = function (nums1, m, nums2, n) {
+  for (let i = 0; i < n; i++) {
+    nums1[m + i] = nums2[i];
+  }
+  nums1.sort((a, b) => a - b);
+
+  console.log(nums1);
+};
+
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
